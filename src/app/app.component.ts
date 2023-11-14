333a824e51583a7a54bc344d26778683167a9e46
Marco Torres <marcoantonio.torres@icloud.com>
2023-11-14 18:34:06 -0500

    Added some setTimeout to our code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,21 +8,25 @@ import { Observable } from 'rxjs';
 })
 export class AppComponent {
   // Steps for an observable
-  // Obserbale [event emitter]
+  // Obserbale [event emitter] (Call Back)
   // Observer  [subscriber]
-  // Handler   [event handler] (CALL BACK)
-
+  // Handler   [event handler] (Call Back)
 
   title = 'playground';
 
-  data: any[] = [];
+  data: number[] = [];
 
+  // oberservable
   myObservable = new Observable((o) => {
-    
+    setTimeout(() => { o.next(1) }, 500)
+    setTimeout(() => { o.next(2) }, 1000)
+    setTimeout(() => { o.next(3) }, 1500)
   })
 
   getData(){
-
+    // Observer [subscriber]
+    this.myObservable.subscribe((o:any) => {   // Event handler Call Back
+      this.data.push(o)
+    })
   }
-
 }
