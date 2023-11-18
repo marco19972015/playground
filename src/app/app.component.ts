2d375732f05f55ba06e82eee7aeb3eb643f825cf
Marco Torres <marcoantonio.torres@icloud.com>
2023-11-18 12:48:13 -0500

    Fixed the deprecated issue using the most recent way to handle an error and complete method. This recent way is done by turning our observer into an object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,18 +14,31 @@ export class AppComponent {
   myObservable = new Observable((observer) => {
     // Set time intervals by 1 second
     setTimeout(() => { observer.next(this.userInput)},)
-    console.log(this.userInputArray);
     setTimeout(() => {observer.error(new Error('This error will be thrown after the first run')), 1000})
+    // setTimeout(() => {observer.complete()})
   })
 
   // method to subscribe nad handle the subscribe 
   getAsynch(){
-    // subscribe to the observable
-    this.myObservable.subscribe((value) => {  // use the handler to push value getting retrieved
-      this.userInputArray.push(value)
-    }, (err) => {
-      alert(err)
-    }
-    )}
+    this.myObservable.subscribe({
+      next: (val: any) => { // remember the arrow function allows us to access array outside of .subscribe scope
+      this.userInputArray.push(val);
+      }, // we go back to methods
+      error(err){
+        alert(err);
+      },
+      complete(){
+        alert('Data is completed and streamed')
+      }
+    })
 
+    // // subscribe, error, complete
+    // // subscribe to the observable
+    // this.myObservable.subscribe((value) => {  // use the handler to push value getting retrieved
+    //   this.userInputArray.push(value)
+    // }, (err) => {
+    //   alert(err)
+    // }
+    // )}
+  }
 }
