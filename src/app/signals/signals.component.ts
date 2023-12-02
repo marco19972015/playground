bc3ced14074c3083c952efb029868cfc6a825a28
Marco Torres <marcoantonio.torres@icloud.com>
2023-12-02 18:17:55 -0500

    Created another component called signals component used to show example of adding to an array using signals

--- src/app/signals/signals.component.ts
+++ src/app/signals/signals.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-signals',
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class SignalsComponent {
 
+  // Property to store user input
+  userInput: string = "";
+
+  // Property to store the user input in array
+  userInputArray = signal<string[]>(['Value 1']);
+
+
+  // Push the input into array signal
+  sigButton(){
+    // Use mutate since array is a mutable type
+    this.userInputArray.mutate((currentValue) => {
+      currentValue.push(this.userInput)
+    })
+
+    console.log(this.userInputArray());
+    
+  }
 }
