44b53dbc278b76db41d415603a3afbf5060b55d3
Marco Torres <marcoantonio.torres@icloud.com>
2023-12-02 17:54:06 -0500

    First commit, this includes the creation of two new components. The logic in component change detections shows how the DoCheck lifecycle hook is called everytime we click out button to append/push our user input into an array

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ChangeDetectionComponent } from './change-detection/change-detection.component';
+import { SignalsComponent } from './signals/signals.component';
 
 @NgModule({
   declarations: [
-    AppComponent, 
+    AppComponent,
+    ChangeDetectionComponent,
+    SignalsComponent, 
     
   ],
   imports: [
--- src/app/change-detection/change-detection.component.ts
+++ src/app/change-detection/change-detection.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-change-detection',
+  templateUrl: './change-detection.component.html',
+  styleUrls: ['./change-detection.component.css']
 })
-export class AppComponent {
-  // The problem with this is that we are running the DoCheck lifeCycle hook in the background
+export class ChangeDetectionComponent {
+    // The problem with this is that we are running the DoCheck lifeCycle hook in the background
 
   // get the user input
   getUserInput: string = ''
@@ -20,4 +19,5 @@ export class AppComponent {
   pushInToarray(){
     this.storingUserInput.push(this.getUserInput);
   }
+
 }
